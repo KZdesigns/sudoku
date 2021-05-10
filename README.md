ea24377cbd3f5988e79e568d519f5be46e0c3cd3
Kyle Zimmerman <kzdesignsios@gmail.com>
2021-05-09 20:20:41 -0700

    tile class finished

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# sudoku
+# Sudoku
+Ruby implementation of a sudoku puzzle solver.
+
+
+## Learning Goals
+- Understand what factory methods are and how to use them
+- Understand how classes interact and how to use require_relative
+- Be able to write and explain how bracket methods [] and []= work
+- Be able to use pry to test small parts of your code
---
+++ tile.rb
@@ -0,0 +1,33 @@
+require 'colorize' #=> requiring in colorize gem
+
+class Tile
+    attr_reader :value #=> giving read only access to value instance variable
+
+    def initialize(value) #=> initializing tile object passing a single value
+        @value = value #=> value instance variable being set to the value passed into Tile.new(value)
+        @given = value == 0 ? false : true #=> given instance variable is a boolen if value passed in is 0 set given to false, otherwise true
+    end
+
+    def color #=> method to assign color to the tile
+        given? ? :blue : :red #=> if given is true tile is :blue otherwise :red
+    end
+
+    def given? #=> getter for given instance variable value (true or false)
+        @given
+    end
+
+    def to_s #=> method turns value into a string
+        value == 0 ? " " : value.to_s.colorize(color)
+        #=> if value equals 0 to_s return " ", otherwise turns the value to a string and uses colorize method and pass the value of color method on line 11
+    end
+
+    def value=(new_value)
+        if given? #=> checking to see if the tile is given
+            puts "Invalid move can't change a value of a given tile"
+            #=> if it is puts message above
+        else #=> if not given set the value instance to the new_value passed in
+            @value = new_value
+        end
+    end
+end
+
